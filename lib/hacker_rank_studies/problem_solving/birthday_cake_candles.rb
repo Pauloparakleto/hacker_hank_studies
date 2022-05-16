697f5b7fa7b525216856ba23b6f978bf7c589d3b
paulo-felipe <alparakleto@gmail.com>
2022-05-16 19:13:23 -0300

    feat(lib/hacker_ranck_studies/birthday_cake_candles.rb): count biggest number

---
+++ lib/hacker_rank_studies/problem_solving/birthday_cake_candles.rb
@@ -0,0 +1,14 @@
+module HackerRankStudies
+    module ProblemSolving
+        class BirthdayCakeCandles
+            def initialize(array)
+                @array = array
+            end
+
+            def count_biggest_number
+                maximum_number = @array.max
+                @array.count maximum_number
+            end
+        end
+    end
+end
