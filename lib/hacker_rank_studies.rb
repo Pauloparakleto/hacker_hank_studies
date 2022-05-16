5a6570daf736ce580cbb8281790ed054c8d2c77b
paulo-felipe <alparakleto@gmail.com>
2022-05-16 19:06:54 -0300

    chore(lib/hacker_ranck_studies.rb): require relative problem_solving birthday cake candles

--- lib/hacker_rank_studies.rb
+++ lib/hacker_rank_studies.rb
@@ -2,6 +2,7 @@
 
 require_relative 'hacker_rank_studies/version'
 require_relative 'hacker_rank_studies/problem_solving/simple_array_sum'
+require_relative 'hacker_rank_studies/problem_solving/birthday_cake_candles'
 
 
 module HackerRankStudies
