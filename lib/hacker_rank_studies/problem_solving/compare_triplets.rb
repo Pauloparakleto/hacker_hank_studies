f83f44d18e08bedddbe70cc19e3bb641371aaa65
paulo-felipe <alparakleto@gmail.com>
2022-05-23 08:42:14 -0300

    feat(compare_triplets.rb): initialize score with zeros

--- lib/hacker_rank_studies/problem_solving/compare_triplets.rb
+++ lib/hacker_rank_studies/problem_solving/compare_triplets.rb
@@ -3,11 +3,16 @@ module HackerRankStudies
         # The problem
         # https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
         class CompareTriplets
-            attr_accessor :first_score, :second_score
+            attr_accessor :first_score, :second_score, :score
 
             def initialize(first_score, second_score)
                 @first_score = first_score
                 @second_score = second_score
+                @score = [0, 0]
+            end
+
+            def compare
+
             end
         end
     end
