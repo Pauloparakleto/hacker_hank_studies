5167e0a8279d3fe70e0f1a37fe49873801ad2ee6
paulo-felipe <alparakleto@gmail.com>
2022-05-23 08:27:19 -0300

    feat(problem_solving/compare_triplets.rb): add initialize and resposible module

---
+++ lib/hacker_rank_studies/problem_solving/compare_triplets.rb
@@ -0,0 +1,14 @@
+module HackerRankStudies
+    module ProblemSolving
+        # The problem
+        # https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
+        class CompareTriplets
+            attr_accessor :first_score, :second_score
+
+            def initialize(first_score, second_score)
+                @first_score = first_score
+                @second_score = second_score
+            end
+        end
+    end
+end
