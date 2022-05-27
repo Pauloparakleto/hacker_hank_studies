7e647bddc0c8d0bd6ba018d535dcbba4a2d18ea0
paulo-felipe <alparakleto@gmail.com>
2022-05-27 08:46:41 -0300

    feat(grading_student.rb): add case when grade is less than standard grade value

--- lib/hacker_rank_studies/problem_solving/grading_students.rb
+++ lib/hacker_rank_studies/problem_solving/grading_students.rb
@@ -1,12 +1,24 @@
 module HackerRankStudies
     module ProblemSolving
         # https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
-        
+
         class GradindStudents
             attr_accessor :original_grades
 
+            STANDARD_GRADE = 38
+
             def initialize(original_grades)
                 @original_grades = original_grades
+            end
+
+            def final_grade
+                original_grades.map do |grade|
+                    grade if is_grade_less_than_standard_grade(grade)
+                end
+            end
+
+            def is_grade_less_than_standard_grade(grade)
+                grade if grade < STANDARD_GRADE
             end
         end
     end
