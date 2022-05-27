8367160dcfc79f3c9b6fbcd3e9dffd5d20496223
paulo-felipe <alparakleto@gmail.com>
2022-05-27 08:45:40 -0300

    test(grading_student_spec.rb): describe final grade when grade is less than standard value

--- spec/problem_solving/grading_students_spec.rb
+++ spec/problem_solving/grading_students_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe HackerRankStudies::ProblemSolving::GradindStudents do
             expect(correct_initialize.original_grades).to eq(original_grades)
         end
     end
+
+    describe '#final_grade' do
+        it '[75, 67, 40, 33]' do
+            expect(correct_initialize.final_grade).to eq([[75, 67, 40, 33]])
+        end
+    end
+
+    describe '#final_grade' do
+        context 'when grade is less than 38' do
+            let(:grade_less_than_38){ [37, 0, 20] }
+            subject(:grade_student_less_than_38) { described_class.new(grade_less_than_38) }
+            
+            it 'returns final grade equal original grade' do
+                expect(grade_student_less_than_38.final_grade).to eq(grade_less_than_38)
+            end
+        end
+    end
 end
