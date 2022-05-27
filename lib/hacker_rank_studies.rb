79ac8e5fabdcd156e50a8bd011de47c0f0e3ab92
paulo-felipe <alparakleto@gmail.com>
2022-05-27 08:54:03 -0300

    chore add require compare triplets

--- lib/hacker_rank_studies.rb
+++ lib/hacker_rank_studies.rb
@@ -3,6 +3,7 @@
 require_relative 'hacker_rank_studies/version'
 require_relative 'hacker_rank_studies/problem_solving/simple_array_sum'
 require_relative 'hacker_rank_studies/problem_solving/birthday_cake_candles'
+require_relative 'hacker_rank_studies/problem_solving/compare_triplets'
 require_relative 'hacker_rank_studies/problem_solving/grading_students'
 
 module HackerRankStudies
