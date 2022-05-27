d0b23c03c923afba713ea0b4efd6d842454b7815
paulo-felipe <alparakleto@gmail.com>
2022-05-27 08:11:29 -0300

    refactor(grading_students_spec.rb): use let original grade variable and subject for all

--- spec/problem_solving/grading_students_spec.rb
+++ spec/problem_solving/grading_students_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe HackerRankStudies::ProblemSolving::GradindStudents do
+    let(:original_grades){ [73, 67, 38, 33] }
+    subject(:correct_initialize){ described_class.new(original_grades) }
+    
     describe '#initialize' do
         context 'when input is correct' do
-            let(:original_grades){ [73, 67, 38, 33] }
-
             it 'is truthy' do
                 expect(described_class.new(original_grades)).to be_truthy
             end
