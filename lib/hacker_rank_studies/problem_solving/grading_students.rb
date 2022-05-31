a0b04f4ed2be1f8a28e60da5c8290c73b3f925c1
paulo-felipe <alparakleto@gmail.com>
2022-05-31 09:49:34 -0300

    feat(grading_students.rb): soved problem, it needs refactoring

--- lib/hacker_rank_studies/problem_solving/grading_students.rb
+++ lib/hacker_rank_studies/problem_solving/grading_students.rb
@@ -13,7 +13,22 @@ module HackerRankStudies
 
             def final_grade
                 original_grades.map do |grade|
-                    grade if is_grade_less_than_standard_grade(grade)
+                    if is_grade_less_than_standard_grade(grade)
+                        grade
+                    else
+                        next_multiple_of5 = (grade/10)*10 + 5
+                    rest = grade%10
+                        if rest > 5
+                            next_multiple_of5 = next_multiple_of5 + 5
+                        end
+                    
+                        diff = next_multiple_of5 - grade
+                        if diff < 3
+                            next_multiple_of5
+                        else
+                            grade
+                        end
+                    end
                 end
             end
 
