62d46a5c1f9279efa2375d17bb8c086ece386313
paulo-felipe <alparakleto@gmail.com>
2022-06-01 07:58:27 -0300

    refactor(grading_students.rb): apply rubocop correction, style and self assignment += to next multiple of five

--- lib/hacker_rank_studies/problem_solving/grading_students.rb
+++ lib/hacker_rank_studies/problem_solving/grading_students.rb
@@ -1,40 +1,39 @@
 module HackerRankStudies
-    module ProblemSolving
-        # https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+  module ProblemSolving
+    # https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
 
-        class GradindStudents
-            attr_accessor :original_grades
+    class GradindStudents
+      attr_accessor :original_grades
 
-            STANDARD_GRADE = 38
+      STANDARD_GRADE = 38
+      STANDARD_DIFFERENCE_BETWEEN_NEXT_MULTIPLE_OF5 = 3
 
-            def initialize(original_grades)
-                @original_grades = original_grades
+      def initialize(original_grades)
+        @original_grades = original_grades
+      end
+
+      def final_grade
+        original_grades.map do |grade|
+          if is_grade_less_than_standard_grade(grade)
+            grade
+          else
+            next_multiple_of5 = (grade / 10) * 10 + 5
+            rest = grade % 10
+            next_multiple_of5 += 5 if rest > 5
+
+            diff = next_multiple_of5 - grade
+            if diff < 3
+              next_multiple_of5
+            else
+              grade
             end
-
-            def final_grade
-                original_grades.map do |grade|
-                    if is_grade_less_than_standard_grade(grade)
-                        grade
-                    else
-                        next_multiple_of5 = (grade/10)*10 + 5
-                    rest = grade%10
-                        if rest > 5
-                            next_multiple_of5 = next_multiple_of5 + 5
-                        end
-                    
-                        diff = next_multiple_of5 - grade
-                        if diff < 3
-                            next_multiple_of5
-                        else
-                            grade
-                        end
-                    end
-                end
-            end
-
-            def is_grade_less_than_standard_grade(grade)
-                grade if grade < STANDARD_GRADE
-            end
+          end
         end
+      end
+
+      def is_grade_less_than_standard_grade(grade)
+        grade if grade < STANDARD_GRADE
+      end
     end
+  end
 end
