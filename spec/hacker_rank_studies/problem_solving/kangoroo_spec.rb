91ba3f10f5f9f719e0521dda8e68c1625cafdf5e
paulo-felipe <alparakleto@gmail.com>
2022-06-04 13:21:03 -0300

    test kangoroo_spec.rb add edge cases

--- spec/hacker_rank_studies/problem_solving/kangoroo_spec.rb
+++ spec/hacker_rank_studies/problem_solving/kangoroo_spec.rb
@@ -25,5 +25,41 @@ RSpec.describe HackerRankStudies::ProblemSolving::Kangoroo do
         .should_same_possition?).to eq('NO')
       end
     end
+
+    context 'when [21, 6, 47, 3]' do
+      let(:first_position) { 21 }
+      let(:first_rate) { 6 }
+      let(:second_position) { 47 }
+      let(:second_rate) { 3 }
+
+      it 'is NO' do
+        expect(described_class.new(first_position, first_rate, second_position, second_rate)
+        .should_same_possition?).to eq('NO')
+      end
+    end
+
+    context 'when [4181, 3976, 6312, 988]' do
+      let(:first_position) { 4181 }
+      let(:first_rate) { 3976 }
+      let(:second_position) { 6312 }
+      let(:second_rate) { 988 }
+
+      it 'is NO' do
+        expect(described_class.new(first_position, first_rate, second_position, second_rate)
+        .should_same_possition?).to eq('NO')
+      end
+    end
+
+    context 'when [1571, 4240, 9023, 4234]' do
+      let(:first_position) { 1571 }
+      let(:first_rate) { 4240 }
+      let(:second_position) { 9023 }
+      let(:second_rate) { 4234 }
+
+      it 'is YES' do
+        expect(described_class.new(first_position, first_rate, second_position, second_rate)
+        .should_same_possition?).to eq('YES')
+      end
+    end
   end
 end
