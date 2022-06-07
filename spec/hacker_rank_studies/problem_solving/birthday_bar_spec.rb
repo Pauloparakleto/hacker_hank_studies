2a36cc157fcb80b72941830e76aeacfb5dc52e76
paulo-felipe <alparakleto@gmail.com>
2022-06-07 19:09:11 -0300

    test(birthday_bar.rb): add initial according to explanation harcker rank examples

---
+++ spec/hacker_rank_studies/problem_solving/birthday_bar_spec.rb
@@ -0,0 +1,35 @@
+RSpec.describe HackerRankStudies::ProblemSolving::BirthdayBar do
+  context 'when day is 3 and month is 2' do
+    let(:day) { 3 }
+    let(:month) { 2 }
+
+    it 'is 2' do
+      chocolate_bar = [1, 2, 1, 3, 2]
+
+      result = described_class.new(chocolate_bar, day, month).count_division_posibilities
+
+      expect(result).to eq(2)
+    end
+
+    it 'is 0' do
+      chocolate_bar = [1, 1, 1, 1, 1]
+
+      result = described_class.new(chocolate_bar, day, month).count_division_posibilities
+
+      expect(result).to eq(0)
+    end
+  end
+
+  context 'when day is 4 and month is 1' do
+    let(:day) { 3 }
+    let(:month) { 2 }
+
+    it 'is 1' do
+      chocolate_bar = [4]
+
+      result = described_class.new(chocolate_bar, day, month).count_division_posibilities
+
+      expect(result).to eq(2)
+    end
+  end
+end
