45122663b6312e8e63815672c1b40c7bc688f98c
paulo-felipe <alparakleto@gmail.com>
2022-06-07 22:46:42 -0300

    refactor(birthday_bar.rb): solve to much assumption to instance variable smell

--- lib/hacker_rank_studies/problem_solving/birthday_bar.rb
+++ lib/hacker_rank_studies/problem_solving/birthday_bar.rb
@@ -5,24 +5,25 @@ module HackerRankStudies
     # Problem description
     # https://www.hackerrank.com/challenges/the-birthday-bar/problem?isFullScreen=true
     class BirthdayBar
-      attr_reader :chocolate_bar, :day, :month
+      attr_reader :chocolate_bar, :day, :month, :counter
 
       def initialize(chocolate_bar, day, month)
         @chocolate_bar = chocolate_bar
         @day = day
         @month = month
+        @counter = counter
       end
 
       def count_division_posibilities
         return chocolate_bar.count(day) if month.eql?(1)
 
-        @counter = 0
+        counter = 0
 
         chocolate_bar.combination(month).to_a.uniq.each do |chocolate|
-          @counter += 1 if chocolate.sum.eql?(day)
+          counter += 1 if chocolate.sum.eql?(day)
         end
 
-        @counter
+        counter
       end
     end
   end
