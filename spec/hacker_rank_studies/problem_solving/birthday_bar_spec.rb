9efac2ed21a14d30bf67cf78dab13cd6235f328c
paulo-felipe <alparakleto@gmail.com>
2022-06-07 23:04:03 -0300

    test(birthday_bar_spec.rb): add case when day is 18 and month is 7

--- spec/hacker_rank_studies/problem_solving/birthday_bar_spec.rb
+++ spec/hacker_rank_studies/problem_solving/birthday_bar_spec.rb
@@ -20,6 +20,19 @@ RSpec.describe HackerRankStudies::ProblemSolving::BirthdayBar do
     end
   end
 
+  context 'when day is 18 and month is 7' do
+    let(:day) { 18 }
+    let(:month) { 7 }
+
+    it 'is 2' do
+      chocolate_bar = [2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1]
+
+      result = described_class.new(chocolate_bar, day, month).count_division_posibilities
+
+      expect(result).to eq(3)
+    end
+  end
+
   context 'when day is 4 and month is 1' do
     let(:day) { 4 }
     let(:month) { 1 }
