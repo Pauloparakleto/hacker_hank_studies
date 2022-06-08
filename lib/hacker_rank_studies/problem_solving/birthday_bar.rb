635a863ac9fd969fdd8f69f7d263b1f32e3ba105
paulo-felipe <alparakleto@gmail.com>
2022-06-07 22:39:03 -0300

    refactor(birthday_bar.rb): instance variables are writable attributes

--- lib/hacker_rank_studies/problem_solving/birthday_bar.rb
+++ lib/hacker_rank_studies/problem_solving/birthday_bar.rb
@@ -5,7 +5,7 @@ module HackerRankStudies
     # Problem description
     # https://www.hackerrank.com/challenges/the-birthday-bar/problem?isFullScreen=true
     class BirthdayBar
-      attr_accessor :chocolate_bar, :day, :month
+      attr_reader :chocolate_bar, :day, :month
 
       def initialize(chocolate_bar, day, month)
         @chocolate_bar = chocolate_bar
