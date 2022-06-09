b62dc68804905950768273a9b3f2f931afd157c3
paulo-felipe <alparakleto@gmail.com>
2022-06-08 22:34:27 -0300

    refactor(birthday_bar.rb): remove too many staqtments spliting method

--- lib/hacker_rank_studies/problem_solving/birthday_bar.rb
+++ lib/hacker_rank_studies/problem_solving/birthday_bar.rb
@@ -5,7 +5,8 @@ module HackerRankStudies
     # Problem description
     # https://www.hackerrank.com/challenges/the-birthday-bar/problem?isFullScreen=true
     class BirthdayBar
-      attr_reader :chocolate_bar, :day, :month, :counter
+      attr_reader :chocolate_bar, :day, :month
+      attr_accessor :counter
 
       def initialize(chocolate_bar, day, month)
         @chocolate_bar = chocolate_bar
@@ -14,18 +15,30 @@ module HackerRankStudies
         @counter = counter
       end
 
+      def out_of_range?(index)
+        (month + index) > chocolate_bar.size
+      end
+
+      def can_divide?(chocolate_bar_peace)
+        chocolate_bar_peace.sum.eql?(day)
+      end
+
       def count_division_posibilities
         return chocolate_bar.count(day) if month.eql?(1)
 
-        counter = 0
+        @counter = 0
 
         chocolate_bar.each_index do |index|
-          break if (month + index) > chocolate_bar.size
-
-          counter += 1 if chocolate_bar[index...(month + index)].sum.eql?(day)
+          count_division(chocolate_bar, index)
         end
 
-        counter
+        @counter
+      end
+
+      def count_division(chocolate_bar, index)
+        chocolate_bar_peace = chocolate_bar[index...(month + index)]
+
+        @counter += 1 if can_divide?(chocolate_bar_peace)
       end
     end
   end
