cdce59eff7f28f7513a78ff52eb15ba21c01f84d
paulo-felipe <alparakleto@gmail.com>
2022-10-15 13:07:36 -0300

    feat bber_man.rb: add initial state and final state equal to initial state when given second is 1

---
+++ lib/hacker_rank_studies/problem_solving/bomber_man.rb
@@ -0,0 +1,17 @@
+module HackerRankStudies
+  module ProblemSolving
+    class BomberMan
+      attr_accessor :n_seconds, :initial_state, :final_state
+
+      def initialize(n_seconds, grid)
+        @n_seconds = n_seconds
+        @initial_state = grid
+        @final_state = @initial_state
+      end
+
+      def call
+        return if n_seconds.eql?(1)
+      end
+    end
+  end
+end
