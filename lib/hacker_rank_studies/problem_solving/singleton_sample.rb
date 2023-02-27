99fd657104541f0aef2b232d3938ed38bbc046b1
paulo-felipe <alparakleto@gmail.com>
2023-02-27 17:06:17 -0300

    singleton_sample.rb and singleton_sample_spec.rb: basic principles, warranty the Object is the same no meter how many times its instance is called. Overriding dump,load, clone and dup. Must be achieved with Marshal class mediation.

--- lib/hacker_rank_studies/problem_solving/singleton_sample.rb
+++ lib/hacker_rank_studies/problem_solving/singleton_sample.rb
@@ -3,9 +3,27 @@
 module HackerRankStudies
   module ProblemSolving
     class SingletonSample
+      attr_accessor :keep, :strip
+
       private_class_method :new
       def self.instance
         @instance ||= new
+      end
+
+      def _dump(depth = -1)
+        ''
+      end
+
+      def _load(str)
+        ''
+      end
+
+      def clone
+        raise TypeError, "can't clone instance of singleton #{self.class}"
+      end
+
+      def dup
+        raise TypeError, "can't dup instance of singleton #{self.class}"
       end
     end
   end
--- spec/hacker_rank_studies/problem_solving/singleton_sample_spec.rb
+++ spec/hacker_rank_studies/problem_solving/singleton_sample_spec.rb
@@ -8,7 +8,10 @@ RSpec.describe HackerRankStudies::ProblemSolving::SingletonSample do
   end
 
   describe "#instance" do
-    # a == b => true
+    it 'returns zero objects in space' do
+      expect(ObjectSpace.each_object(described_class){}).to eql(0)
+    end
+
     it 'returns described class' do
       expect(described_class.instance).to be_an_instance_of(
                                             HackerRankStudies::ProblemSolving::SingletonSample
@@ -22,15 +25,86 @@ RSpec.describe HackerRankStudies::ProblemSolving::SingletonSample do
       expect(a).to eql(b)
     end
 
-    it 'returns zero objects in space' do
-      expect(ObjectSpace.each_object(described_class){}).to eql(0)
-    end
-
     it 'returns 1 object in space' do
       described_class.instance
       described_class.instance
 
       expect(ObjectSpace.each_object(described_class){}).to eql(1)
+    end
+  end
+
+  describe "#clone" do
+    it "raises TypeError" do
+      a = described_class.instance
+
+      expect{ a.clone }.to raise_error(TypeError)
+    end
+  end
+
+  describe "#dup" do
+    it "raises TypeError" do
+      a = described_class.instance
+
+      expect{ a.dup }.to raise_error(TypeError)
+    end
+  end
+
+  describe "#_dump" do
+    it 'returns empty string' do
+      expect(described_class.instance._dump).to eql("")
+    end
+  end
+
+  describe "#keep=" do
+    it 'returns variable set' do
+      a = described_class.instance
+
+      expect(a.keep = "Keep this").to eql("Keep this")
+    end
+  end
+
+  describe "#keep" do
+    it 'gets value' do
+      a = described_class.instance
+      a.keep = "Keep this"
+
+      expect(a.keep).to eql("Keep this")
+    end
+  end
+
+  describe "#strip=" do
+    it 'returns variable set' do
+      a = described_class.instance
+
+      expect(a.strip = "Get rid of this").to eql("Get rid of this")
+    end
+  end
+
+  describe "#strip" do
+    it 'gets value' do
+      a = described_class.instance
+      a.strip = "Get rid of this"
+
+      expect(a.strip).to eql("Get rid of this")
+    end
+  end
+
+  describe "#Marchal.dump" do
+    xit "conditions" do
+      a = described_class.instance
+      a.keep = "Keep this"
+      a.strip = "Get rid of this"
+
+      stored_state = Marshal.dump(a)
+
+      a.keep = nil
+      a.strip = nil
+
+      b = Marshal.load(stored_state)
+
+      expect(a).to eql(b)
+      expect(a.keep).to eql("Keep this")
+      expect(a.strip).to be_nil
     end
   end
 end
