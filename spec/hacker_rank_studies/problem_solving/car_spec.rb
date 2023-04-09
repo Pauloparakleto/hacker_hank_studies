fd5262b43c6e78d4a8ff2fe2f6f490f8ff7f2090
paulo-felipe <alparakleto@gmail.com>
2023-04-09 12:20:16 -0300

    test (car_spec.rb): add start_engine and stop_engine. At this point, car is coupled with the engine. But if another type of vehicle pop up, an air plain, which clearly has in common start and stop its own engine, we have the necessity of abstracting the engine function.

---
+++ spec/hacker_rank_studies/problem_solving/car_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# The Design Pattern in Ruby chapter 1 topic Preffer composition over inheritance
+# The main idea here it if the objective is building software uncoupled where we can
+# change things isolated, relying only in inheritance has a problem since a single change
+# in super class would affect all its subclasses.
+# The example here is about a vehicle composition rather than an inheritance.
+
+RSpec.describe HackerRankStudies::ProblemSolving::Car do
+  describe 'start_engine' do
+    it 'stars engine' do
+      expect(described_class.new.start_engine).to eq('Engine started')
+    end
+
+    it 'stops engine' do
+      expect(described_class.new.stop_engine).to eq('Engine stopped')
+    end
+  end
+end
