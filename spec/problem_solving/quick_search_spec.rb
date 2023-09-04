cd8a3b55ea7acdcebd864f8a64d430741ce87408
Paulo Felipe Souza <alparakleto@gmail.com>
2023-09-03 21:16:51 -0300

    refactor quick_sort_spec.rb: prepare to add index as second argument

--- spec/problem_solving/quick_search_spec.rb
+++ spec/problem_solving/quick_search_spec.rb
@@ -1,13 +1,15 @@
 RSpec.describe 'QuickSearch' do
-    def particion(array)
-        pivot_index = 0
-        pivot = array[pivot_index]
+    FIRST_INDEX = 0
+
+    def particion(array, pivot_index = nil)
+        @pivot_index = pivot_index || FIRST_INDEX
+        pivot = array[@pivot_index]
         i = 1
         j = 0
         array_length =  array.length - 1
 
         for i in i..array_length
-            if array[i] < pivot
+            if array[i] <= pivot
                 j += 1
                 element_to_swith_a = array[i]
                 element_to_swith_b = array[j]
@@ -15,14 +17,20 @@ RSpec.describe 'QuickSearch' do
                 array[i] = element_to_swith_b
             end
         end
-        array[pivot_index] = array[j]
+        array[@pivot_index] = array[j]
         array[j] = pivot
         array
     end
 
     let(:array) { [3, 8, 7, 10, 0, 23, 2, 1, 77, 7] }
+    let(:array_second) { [7, 8, 1, 2, 90, 4, 65, 32] }
 
     it 'has particioned' do
         expect(particion(array)).to eq([1, 0, 2, 3, 8, 23, 7, 10, 77, 7])
+    end
+
+    it 'has particioned chossing 7 as the pivot' do
+        index = 0
+        expect(particion(array_second, index)).to eq([4, 1, 2, 7, 90, 8, 65, 32])
     end
 end
